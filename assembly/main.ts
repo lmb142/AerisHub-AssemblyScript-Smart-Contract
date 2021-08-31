7a10c713c4c5b819303078b6b7dc379a5bc8bcb8
Lucas Bulczynski <85247135+lmb142@users.noreply.github.com>
2021-08-31 09:52:28 -0400

    Tue Morning

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,72 +1,82 @@
 import { storage, logging } from "near-sdk-as";
 
-// This is the smart contract itself. Here we need to define the cathode struct, as well as functions that transfer ownership back and forth. 
-//We should use the rust example to copy the architecture 
+//***** 
+//DEFINE AERISHUB UNIQUE ID AS A GLOBAL CONSTANT HERE. THE REASON FOR THIS 
+//IS SO THAT WAY IN THE EVENT OF PIRACY/MALICIOUS CONTRACT USE, OUR SECURITY CHECKS 
+//CAN HAVE CATHODES BE PRESERVED BY AERISHUB TAKING OWNERSHIP OF THEM. 
 
-/*
-export function incrementCounter(value: i32): void {
-  const newCounter = storage.getPrimitive<i32>("counter", 0) + value;
-  storage.set<i32>("counter", newCounter);
-  logging.log("Counter is now: " + newCounter.toString());
-}
+//*****
+//HERE ONCE WE SOLIDIFY OUR DATA INFLOW, WE WILL CALL THE CATHODE CONSTRUCTOR WHICH WILL 
+//CREATE THE CATHODE "OBJECT", INPUTTING ALL THE NECESSARY DATA
 
-export function decrementCounter(value: i32): void {
-  const newCounter = storage.getPrimitive<i32>("counter", 0) - value;
-  storage.set<i32>("counter", newCounter);
-  logging.log("Counter is now: " + newCounter.toString());
-}
-
-export function getCounter(): i32 {
-  return storage.getPrimitive<i32>("counter", 0);
-}
-
-export function resetCounter(): void {
-  storage.set<i32>("counter", 0);
-  logging.log("Counter is reset!");
-}
-*/
-
-
-//we need some sort of an outside owner variable that is brought in. this would deal with the API data coming in, so im not super sure as to how this would work
-
+//This class below could also easily be used for bundles of cathodes. 
 export class Cathode { 
-  //defining cathode class variables A(im assuming 32 bit signed integer for now)
-  uniqueid: i32; //lets look into the size of our uniqueid/hash output. Depending on size, we could have a larger or smaller integer, which is more or less gas storage costs. 
-  owner: i32; //we need to have some form of an aerishub account id for this
-  //do we even want anything else in our cathode class
-  constructor(uniqueid: i32, owner: i32){
+  
+  uniqueid: i32; 
+  ownerid: i32; 
+  tokenValue: i32; //This is the appriasal value of the Cathode/Bundle of Cathodes
+  
+  //*****
+  //DEFINE OTHER CATHODE CHARACTERISTICS HERE
+
+  //This below constructs the actual Cathode/Bundles of Cathodes.
+  constructor(uniqueid: i32, ownerid: i32, tokenValue: i32;){
     this.uniqueid = uniqueid;
-    this.owner = owner;
+    this.ownerid = ownerid;
+    this.tokenValue = tokenValue;
   }
-
-
 }
 
-//we need something that creates the cathode with our owner id, then a part of the contract that deals with exchange of ownership
+//*****
+//HERE ONCE WE SOLIDIFY OUR DATA INFLOW FOR CUSTOMER TRANSACTIONS, WE WILL CALL THE NEWOWNERCHECK CLASS WHICH WILL 
+//STORE THE UNIQUE ID OF THE NEW OWNER. PART OF THIS DATA INFLOW WILL INCLUDE THE CATHODE UNIQUE ID FOR
+//THE CATHODE THEY WISH TO PURCHASE(AS PART OF THE INTERACTION BETWEEN THE FRONT END, BACK END, AND HASH FUNCTION). 
 
-export function grantAccess();{
-    
+
+//when a customer puts in their own info into the website to purchase a Cathode
+//or bundle of Cathodes, this keeps track of their information and makes sure that 
+//the transfer of ownership is successful. If not, the checkAccess function  will 
+//take the proper measures to insure that the Cathode/Cathoide Bundles do not get lost. 
+
+export class NewOwnerCheck (newOwner: i32); void {
+      storeNewOwnerId: i32;
+      constructor(storeNewOwnerId: i32){
+        this.storeNewOwnerId  =  newOwner;
+      }
 }
 
-export function revokeAccess();{
-
+export function transferOfOwnership(Cathode1: Cathode, newOwner: i32); void {
+  
+  let Cathode1.ownerid = newOwner;
+    //implement the necessary security checks here. Likely this will be a Rust not AssemblyScript task.
+    checkAccess(Cathode1.ownerid);
 }
 
-export function transferFrom();{
 
+export function checkAccess(Cathode1.ownerid: Cathode.ownerid); void{
+      
+    switch (storeNewOwnerId) { //This checks if the ownerid for Cathode1 (target Cathode) matches the newOwner Id stored in the NewOwnerCheck class.  
+        case newOwner:
+            
+        break; //If the storeNewOwnerId == Cathode1.ownerid, then we break this switch statement and the transfer is complete
+
+      //here we want to implement some functionality that will notify Aerishub Management
+      //about this faulty nature of the Smart Contract as a default measure, the Contract will be reverted to AerisHub's control
+      //for safe-keeping in the event the smart contract has failed. 
+
+      let Cathode1.owner.id = //predefined AerisHub uniqueid. This will notify AerisHub management as well. 
+
+      }
+
+      //*****
+      //THIS IS THE END OF THE CHECK ACCESS FUNCTION. HERE WE PLAN TO IMPLEMENT A CONNECTION BETWEEN THE BACK END AND FRONT END, IN WHICH
+      //A SALES CONTRACT IS GENERATED, USING DATA ABOUT THE CATHODE (GOING BACKWARDS INTO THE HASH FUNCTION). 
+      
+      
+      //*******we also should talk more about this stage of the sales process, and how much blockchain is involved past this point.*******
 }
 
-export function transfer();{
 
-}
-
-export function checkAccess();{
-
-}
-
-export function getTokenOwner();{
-
-}
 
 
 
